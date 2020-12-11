bb99496de6c38b242048a2431fb00865c3790ef3
djviodes <djviodes26@gmail.com>
2020-12-11 08:16:37 -0800

    Finished the projects model and router

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,15 @@
-// build your `Project` model here
+const db = require('../../data/dbConfig');
+
+module.exports = {
+    find,
+    create,
+};
+
+function find() {
+    return db('projects');
+}
+
+async function create(project) {
+    const [id] = await db('projects').insert(project)
+    return db('projects').where({ id }).first()
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,30 @@
-// build your `/api/projects` router here
+const router = require('express').Router();
+
+const Projects = require('./model');
+
+router.get('/', (req, res) => {
+    Projects.find()
+        .then(projects => {
+            res.status(200).json(projects)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: err.message,
+                stack: err.stack
+            });
+        });
+});
+
+router.post('/', (req, res) => {
+    const newProject = req.body;
+    Projects.create(newProject)
+        .then(project => {
+            res.status(201).json(project)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: err.message,
+                stack: err.stack
+            });
+        });
+});
