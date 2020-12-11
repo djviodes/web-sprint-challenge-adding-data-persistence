0226efeb9beda420513e18d134fd39b250d64c4d
djviodes <djviodes26@gmail.com>
2020-12-11 08:49:51 -0800

    More debugging

--- api/task/model.js
+++ api/task/model.js
@@ -8,7 +8,7 @@ module.exports = {
 function find() {
     return db('tasks')
         .leftJoin('projects', 'projects.id', 'tasks.project_id')
-        .select('tasks.id', 'tasks.description', 'projects.name', 'tasks.notes', 'tasks.completed');
+        .select('tasks.id', 'projects.name', 'projects.description', 'tasks.description', 'tasks.notes', 'tasks.completed');
 }
 
 async function create(task) {
