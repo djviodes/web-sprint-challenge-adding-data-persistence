d968dabf3f217b364e46a33a971e094a531fb28b
djviodes <djviodes26@gmail.com>
2020-12-11 08:06:49 -0800

    Finished the resources router and model

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,15 @@
-// build your `Resource` model here
+const db = require('../../data/dbConfig');
+
+module.exports = {
+    find,
+    create,
+};
+
+function find() {
+    return db('resources');
+}
+
+async function create(resource) {
+    const [id] = await db('resources').insert(resource)
+    return db('resources').where({ id }).first()
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,30 @@
-// build your `/api/resources` router here
+const router = require('express').Router();
+
+const Resources = require('./model');
+
+router.get('/', (req, res) => {
+    Resources.find()
+        .then(resources => {
+            res.status(200).json(resources)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: err.message,
+                stack: err.stack
+            });
+        });
+});
+
+router.post('/', (req, res) => {
+    const newResource = req.body;
+    Resources.create(newResource)
+        .then(resource => {
+            res.status(201).json(resource)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: err.message,
+                stack: err.stack
+            });
+        });
+});
